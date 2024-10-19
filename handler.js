ffb3392b5e75bd3c67d7bf1b79a625e13684276a
axelaquino3 <axelaquino@google.com>
2024-10-18 22:40:26 -0400

    trouble shooting handler.js, trying to get response

--- handler.js
+++ handler.js
@@ -18,26 +18,26 @@ module.exports.sendEmail = async (event) => {
 }
 
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
+// app.get("/", (req, res, next) => {
+//   return res.status(200).json({
+//     message: "Hello from root!",
+//   });
+// });
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
+// app.get("/hello", (req, res, next) => {
+//   return res.status(200).json({
+//     message: "Hello from path!",
+//   });
+// });
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-
+// app.use((req, res, next) => {
+//   return res.status(404).json({
+//     error: "Not Found",
+//   });
+// });
 
 
 
-exports.handler = serverless(app);
+
+
+// exports.handler = serverless(app);
