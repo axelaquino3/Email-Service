3302ce0d617bc8a7edea554d5c50175c46d09730
axelaquino3 <axelaquino@google.com>
2024-10-21 17:21:28 -0400

    retrying edit workflow to deploy directly

--- handler.js
+++ handler.js
@@ -17,27 +17,3 @@ module.exports.sendEmail = async (event) => {
   return response;
 }
 
-
-// app.get("/", (req, res, next) => {
-//   return res.status(200).json({
-//     message: "Hello from root!",
-//   });
-// });
-
-// app.get("/hello", (req, res, next) => {
-//   return res.status(200).json({
-//     message: "Hello from path!",
-//   });
-// });
-
-// app.use((req, res, next) => {
-//   return res.status(404).json({
-//     error: "Not Found",
-//   });
-// });
-
-
-
-
-
-// exports.handler = serverless(app);
