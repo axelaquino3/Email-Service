7694a14d9702a3b9ff421ba245e497a73dd3c893
axelaquino3 <axelaquino@google.com>
2024-10-22 22:32:26 -0400

    Update serverless configuration

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-exports.sendEmail = async (event) => {
+module.exports.sendEmail = async (event) => {
   console.log('Received event:', event);
 
   const response = {
