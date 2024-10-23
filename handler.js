646f3a2b833ae04e1f48f62493010c25c423d22b
axelaquino3 <axelaquino@google.com>
2024-10-23 16:04:57 -0400

    added iam role to yml and email params in handler

--- handler.js
+++ handler.js
@@ -1,20 +1,27 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
-
+// top of file
+const AWS = require("aws-sdk");
+const ses = new AWS.SES({ region: "us-west-2" });
 
 module.exports.sendEmail = async (event) => {
-  console.log('Received event:', event);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',   
-
-    }),
+  // inside the `exports.handler.sendEmail` function, before the `return` statement
+  const params = {
+    Destination: {
+      ToAddresses: ['aquino.axel@gmail.com'], // This should be your email address
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "This is a message generated automatically from a Lambda function.",
+        },
+      },
+      Subject: {
+        Data: "Hello from Lambda",
+      },
+    },
+    Source: "aquino.axel@gmail.com", // This is the email listed in sender. Set it to your email for this practice
   };
+  await ses.sendEmail(params).promise();
+  // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${params.Destination.ToAddresses}`
 
   return response;
-}
-
-
+};
