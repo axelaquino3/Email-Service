2ab37c0b65b51b3eceee0459a45c16a3c2c5ae65
axelaquino3 <axelaquino@google.com>
2024-10-23 17:27:06 -0400

    made edits to handler.js, forgot the response obj

--- handler.js
+++ handler.js
@@ -1,12 +1,13 @@
-// top of file
 const AWS = require("aws-sdk");
 const ses = new AWS.SES({ region: "us-west-2" });
 
 module.exports.sendEmail = async (event) => {
+  console.log("Received event:", event);
+
   // inside the `exports.handler.sendEmail` function, before the `return` statement
   const params = {
     Destination: {
-      ToAddresses: ['aquino.axel@gmail.com'], // This should be your email address
+      ToAddresses: ["aquino.axel@gmail.com"], // This should be your email address
     },
     Message: {
       Body: {
@@ -18,10 +19,17 @@ module.exports.sendEmail = async (event) => {
         Data: "Hello from Lambda",
       },
     },
-    Source: "aquino.axel@gmail.com", // This is the email listed in sender. Set it to your email for this practice
+    Source: "aquino.axel@gmail  .com", // This is the email listed in sender. Set it to your email for this practice
   };
   await ses.sendEmail(params).promise();
   // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${params.Destination.ToAddresses}`
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Email sent to ${params.Destination.ToAddresses}`,
+    }),
+  };
 
   return response;
 };
